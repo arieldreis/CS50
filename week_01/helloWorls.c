7124d6b84adb629545662e40339a5004090939b4
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-15 14:31:02 -0300

    Update launch configuration and enhance helloWorls.c with user input

--- week_01/helloWorls.c
+++ week_01/helloWorls.c
@@ -1,5 +1,9 @@
 # include <stdio.h>
 int main (void){
+    printf("Hello, \"World!\"\n");
     printf("Hello, World!\n");
+    char name[] = "World";
+    printf("Hello, %s!\n", name);
+    get_string("Enter your name: ", name, sizeof(name));
     return 0;
 }
