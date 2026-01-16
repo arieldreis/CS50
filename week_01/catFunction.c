8d1ade61cfbf7091088e7f34866436f097420f4c
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-16 18:24:54 -0300

    Refactor code structure for improved readability and maintainability

--- week_01/catFunction.c
+++ week_01/catFunction.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
+int get_positive_int();
 void meow(int n);
 
+
 int main(int n){
-    do
+    int times = get_positive_int();
+    meow(times);  
+}
+
+
+int get_positive_int(void){
+    int n;
+   do
     {
         int n = get_int("Number");
     } while(n < 1);
-    meow(n);
 }
+
+void meow(int n){
+    for (int i = 0; i < n; i++)
+    {
+        printf("Meow!!!\n");
+    }
+    
+}; 
