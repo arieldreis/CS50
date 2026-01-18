a614e2054454f7d2909f0b76fbcf97dd8e5d7cb4
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-18 20:10:10 -0300

    Refactor mario1.c for improved structure and readability

--- week_01/exercises/mario1.c
+++ week_01/exercises/mario1.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-#include <cs50.h>
+// #include <cs50.h>
 void print_row(int n_User);
 
 int main(void){
     int n_User;
-    int espacos;
     do{
          n_User = get_int("Height: ");
-    }while(n_User < 1);
-    for(int i = 1; i <= n_User; i++){
+    } while(n_User < 1);
+
+    for(int i = 0; i < n_User; i++){
         print_row(n_User);
-        printf(" ");
     }
-    printf("\n");
 }
 
 void print_row(int n_User){
-    for(int i = 1; i <= n_User; i++){
-         printf("");
-         for(int j = 1; j <= n_User; j++){
+    for(int j = 0; j < n_User; j++){
+        printf(" ");
+        for(int k = 0; k <= j; k++){
             printf("#");
-         }
+        }
     }
     printf("\n");
 }
