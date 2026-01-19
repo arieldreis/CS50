c95072c3d2ae3b379421e2bf67e51466bba39e40
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-19 19:18:59 -0300

    New file of cash.c

--- week_01/exercises/mario1.c
+++ week_01/exercises/mario1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// #include <cs50.h>
+#include <cs50.h>
 void print_row(int n_User);
 
 int main(void){
@@ -8,17 +8,21 @@ int main(void){
          n_User = get_int("Height: ");
     } while(n_User < 1);
 
+    int espacos = n_User - 1;
+    int hashtags = n_User + 1;
     for(int i = 0; i < n_User; i++){
         print_row(n_User);
     }
 }
 
 void print_row(int n_User){
+    int qtd = 1;
     for(int j = 0; j < n_User; j++){
-        printf(" ");
-        for(int k = 0; k <= j; k++){
+        for(int k = 0; k < qtd; k++){
             printf("#");
         }
+        qtd += 1;
+        printf("\n");
     }
-    printf("\n");
 }
+
