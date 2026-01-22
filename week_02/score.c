160c97a9d690ffcd89e47d3c5140de6f94e04622
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-22 12:58:13 -0300

    Refactor code structure for improved readability and maintainability

---
+++ week_02/score.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// #include <cs50.h>
+
+double average(int length[], int N){
+    int sum = 0;
+    for(int index = 0; index < N; index++){
+        sum += length[index];
+    }
+
+    double media = (double) sum / N;
+
+    return media;
+}
+
+
+int main(void){
+    // Arrays
+    const int N = 3;
+    int scores[N];
+
+    for(int index = 0; index < N; index++){
+        scores[index] = get_int("Score: ");
+    }
+
+    double funAvg = average(scores, N);
+    printf("Average: %f\n", funAvg);
+}
