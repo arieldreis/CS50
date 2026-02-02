86e526b5c35a478ce5a9a047f74be6658d067199
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-02-02 18:32:47 -0300

    Refactor winner function to iterate over user inputs instead of alphabet array for score calculation

--- week_02/exercises/scrabble.js
+++ week_02/exercises/scrabble.js
@@ -9,26 +9,33 @@ let pontos = [1, 3, 3, 2, 1, 4, 2, 4,
 const winner = (user1, user2) => {
     let contador1 = 0;
     let contador2 = 0;
-    for(let index = 0; index < alfabeto.length; index++){
+
+    for(let index = 0; index < user1.length; index++){
         let indexLetra1 = user1[index];
         let contadorLetra1 = alfabeto.indexOf(indexLetra1); // Index of letters
 
         if(indexLetra1 != undefined){
              console.log(`${indexLetra1} => ${contadorLetra1} => ${pontos[contadorLetra1]}`);
+             contador1 += pontos[contadorLetra1];
         }
-
-        contador1 += pontos[contadorLetra1];
     }
     console.log("------------------------------------");
-    for(let index = 0; index < alfabeto.length;index++){
+    for(let index = 0; index < user2.length; index++){
         let indexLetra2 = user2[index];
         let contadorLetra2 = alfabeto.indexOf(indexLetra2); // Index of letters
 
         if(indexLetra2 != undefined){
              console.log(`${indexLetra2} => ${contadorLetra2} => ${pontos[contadorLetra2]}`);
+             contador2 += pontos[contadorLetra2];
         }
-
-        contador2 += pontos[contadorLetra2];
+    }
+    console.log("------------------------------------");
+    if(contador1 > contador2){
+        console.log("Player 1 wins!");
+    } else if(contador2 > contador1) {
+        console.log("Player 2 wins!");
+    }else{
+        console.log("It's a tie.")
     }
     console.log("------------------------------------");
     console.log("Player 1: "  + contador1);
@@ -36,6 +43,6 @@ const winner = (user1, user2) => {
 };
 
 let user1 = "COMPUTER".toLowerCase();
-let user2 = "Red".toLowerCase();
+let user2 = "BOLIGRAFO".toLowerCase();
 // Calling the function
 winner(user1, user2);
